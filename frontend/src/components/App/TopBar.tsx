6fe2dee789dd9c471a466b317f5613a41646bdc1
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-10-29 17:09:15 -0400

    aksd: Use AKS version for the topbar dropdown menu

--- frontend/src/components/App/TopBar.tsx
+++ frontend/src/components/App/TopBar.tsx
@@ -31,7 +31,7 @@ import React, { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { getProductName, getVersion } from '../../helpers/getProductInfo';
+import { getAksDesktopVersion, getProductName, getVersion } from '../../helpers/getProductInfo';
 import { logout } from '../../lib/auth';
 import { useCluster, useClustersConf } from '../../lib/k8s';
 import { clusterRequest } from '../../lib/k8s/api/v1/clusterRequests';
@@ -382,7 +382,7 @@ export const PureTopBar = memo(
             <Icon icon="mdi:information-outline" />
           </ListItemIcon>
           <ListItemText>
-            {getProductName()} {getVersion()['VERSION']}
+            {getProductName()} {getAksDesktopVersion() ?? getVersion()['VERSION']}
           </ListItemText>
         </MenuItem>
       </Menu>
